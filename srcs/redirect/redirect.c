15e477f7f9997e8b6773132daf26fcfd8224a617
muiida <flashingwind@gmail.com>
2025-07-24 19:21:41 +0900

    fix:t_redirect->error_msgの初期化

--- srcs/redirect/redirect.c
+++ srcs/redirect/redirect.c
@@ -6,11 +6,12 @@
 /*   By: muiida <muiida@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 17:08:00 by tsukuru           #+#    #+#             */
-/*   Updated: 2025/06/23 23:12:09 by muiida           ###   ########.fr       */
+/*   Updated: 2025/07/24 19:20:42 by muiida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../env/env.h"
+#include "env/env.h"
+#include "libft.h"
 #include "minishell.h"
 #include "redirect.h"
 
@@ -19,7 +20,7 @@ t_redirect	*create_redirect(int type, char *file)
 	t_redirect	*redirect;
 	char		*expanded_file;
 
-	redirect = (t_redirect *)malloc(sizeof(t_redirect));
+	redirect = (t_redirect *)ft_calloc(1, sizeof(t_redirect));
 	if (!redirect)
 		return (NULL);
 	redirect->type = type;
@@ -31,7 +32,6 @@ t_redirect	*create_redirect(int type, char *file)
 	}
 	redirect->file = expanded_file;
 	redirect->original_fd = -1;
-	redirect->next = NULL;
 	return (redirect);
 }
 
